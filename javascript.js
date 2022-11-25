8409188efbd780792cc758d198136a57b9cf57bf
qsnumberonefan <rachelcmkim@gmail.com>
2022-11-25 15:51:30 -0800

    initial commit
    for 11/30 deadline

---
+++ javascript.js
@@ -0,0 +1,98 @@
+var whoButtonContainer = document.getElementById('who-container');
+
+var whoHiddenDiv = document.getElementById('hidden-who');
+
+// ✅ Show hidden DIV on hover
+whoButtonContainer.addEventListener('mouseover', function handleMouseOver() {
+  whoHiddenDiv.style.display = 'block';
+});
+
+// ✅ (optionally) Hide DIV on mouse out
+whoButtonContainer.addEventListener('mouseout', function handleMouseOut() {
+  whoHiddenDiv.style.display = 'none';
+});
+
+var whatButtonContainer = document.getElementById('what-container');
+
+var whatHiddenDiv = document.getElementById('hidden-what');
+
+// ✅ Show hidden DIV on hover
+whatButtonContainer.addEventListener('mouseover', function handleMouseOver() {
+  whatHiddenDiv.style.display = 'block';
+});
+
+// ✅ (optionally) Hide DIV on mouse out
+whatButtonContainer.addEventListener('mouseout', function handleMouseOut() {
+  whatHiddenDiv.style.display = 'none';
+});
+
+var whenButtonContainer = document.getElementById('when-container');
+
+var whenHiddenDiv = document.getElementById('hidden-when');
+
+// ✅ Show hidden DIV on hover
+whenButtonContainer.addEventListener('mouseover', function handleMouseOver() {
+  whenHiddenDiv.style.display = 'block';
+});
+
+// ✅ (optionally) Hide DIV on mouse out
+whenButtonContainer.addEventListener('mouseout', function handleMouseOut() {
+  whenHiddenDiv.style.display = 'none';
+});
+
+var whereButtonContainer = document.getElementById('where-container');
+
+var whereHiddenDiv = document.getElementById('hidden-where');
+
+// ✅ Show hidden DIV on hover
+whereButtonContainer.addEventListener('mouseover', function handleMouseOver() {
+  whereHiddenDiv.style.display = 'block';
+});
+
+// ✅ (optionally) Hide DIV on mouse out
+whereButtonContainer.addEventListener('mouseout', function handleMouseOut() {
+  whereHiddenDiv.style.display = 'none';
+});
+
+var whyButtonContainer = document.getElementById('why-container');
+
+var whyHiddenDiv = document.getElementById('hidden-why');
+
+// ✅ Show hidden DIV on hover
+whyButtonContainer.addEventListener('mouseover', function handleMouseOver() {
+  whyHiddenDiv.style.display = 'block';
+});
+
+// ✅ (optionally) Hide DIV on mouse out
+whyButtonContainer.addEventListener('mouseout', function handleMouseOut() {
+  whyHiddenDiv.style.display = 'none';
+});
+
+var howButtonContainer = document.getElementById('how-container');
+
+var howHiddenDiv = document.getElementById('hidden-how');
+
+// ✅ Show hidden DIV on hover
+howButtonContainer.addEventListener('mouseover', function handleMouseOver() {
+  howHiddenDiv.style.display = 'block';
+});
+
+// ✅ (optionally) Hide DIV on mouse out
+howButtonContainer.addEventListener('mouseout', function handleMouseOut() {
+  howHiddenDiv.style.display = 'none';
+});
+
+
+var aboutButtonContainer = document.getElementById('about-container');
+
+var aboutHiddenDiv = document.getElementById('hidden-about');
+
+// ✅ Show hidden DIV on hover
+aboutButtonContainer.addEventListener('mouseover', function handleMouseOver() {
+  aboutHiddenDiv.style.display = 'block';
+});
+
+// ✅ (optionally) Hide DIV on mouse out
+aboutButtonContainer.addEventListener('mouseout', function handleMouseOut() {
+  aboutHiddenDiv.style.display = 'none';
+});
